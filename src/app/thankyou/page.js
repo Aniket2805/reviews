f60eb9da95b1abd273e2c5514fc7923b34e0f3db
Aniket2805 <aniketrocks2468@gmail.com>
2024-05-25 13:58:44 +0530

    w

---
+++ src/app/thankyou/page.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-slate-300">
+      <div className="p-10 sm:p-20 bg-white text-2xl sm:text-4xl rounded-2xl">
+        <h1 className="text-center">Thank you for submitting review</h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
